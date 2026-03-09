a3e61f38c9705be98f00c68235a2f0c519cb8d0f
YIEMM <110466734+YIEMM@users.noreply.github.com>
2026-03-09 21:36:41 +0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-msbuild winform_block.sln /p:Configuration=Release /p:Platform=x64
-msbuild winform_block.sln /p:Configuration=Release /p:Platform=win32
+msbuild Sysmenu_Disable.sln /p:Configuration=Release /p:Platform=x64
+msbuild Sysmenu_Disable.sln /p:Configuration=Release /p:Platform=win32
--- sysmenu_hook/sysmenu_hook.cpp
+++ sysmenu_hook/sysmenu_hook.cpp
@@ -1,4 +1,4 @@
-#include <windows.h>
+#include <Windows.h>
 #include "sysmenu_hook.h"
 
 static WNDPROC g_originalWndProc = nullptr;
